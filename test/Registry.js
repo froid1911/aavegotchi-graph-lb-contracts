e388f009194b213c4d8cf980dc1247e082b8a211
Frank <frank.pfeift@protonmail.com>
2021-12-08 11:57:38 +0100

    added test for get servers

--- test/Registry.js
+++ test/Registry.js
@@ -4,6 +4,9 @@ const { ethers } = require("hardhat");
 describe("Registry contract", function () {
 
     let owner, hardhatRegistry;
+    const subgraphName = "aavegotchi-core-matic";
+    const server = "abc";
+    const subgraphHash = "abc";
 
     it("Deployment should set owner of contract to deployer address", async function () {
         [owner] = await ethers.getSigners();
@@ -14,26 +17,26 @@ describe("Registry contract", function () {
     });
 
     it("Emits SubgraphDeployed event if subgraph hash is added", async function () {
-        const subgraphName = "aavegotchi-core-matic";
-        const subgraphHash = "abc";
         await expect(hardhatRegistry.publishSubgraph(subgraphName, subgraphHash))
             .to.emit(hardhatRegistry, 'SubgraphDeployed')
             .withArgs(subgraphName, subgraphHash);
     })
 
     it("Emits ServerAdded event if server is added", async function () {
-        const subgraphName = "aavegotchi-core-matic";
-        const server = "abc";
         await expect(hardhatRegistry.registerServer(subgraphName, server))
             .to.emit(hardhatRegistry, 'ServerAdded')
             .withArgs(subgraphName, server);
     })
 
     it("Doesn't emit ServerAdded event if server is already added", async function () {
-        const subgraphName = "aavegotchi-core-matic";
-        const server = "abc";
         await expect(hardhatRegistry.registerServer(subgraphName, server))
             .to.not.emit(hardhatRegistry, 'ServerAdded')
             .withArgs(subgraphName, server);
+    })
+
+    it("Fetches Servers for specific subgraph", async function () {
+        const subgraphName = "aavegotchi-core-matic";
+        let servers = await hardhatRegistry.getServers(subgraphName)
+        expect(servers).to.contain(server)
     })
 });
